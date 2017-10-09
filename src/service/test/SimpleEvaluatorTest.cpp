1a0e3be2acc37f5519d568943d22ef012dbcab48
zelinf <fengzlin@mail2.sysu.edu.cn>
2017-10-09 15:05:56 +0800

    Added basic test cases for derivation

--- src/service/test/SimpleEvaluatorTest.cpp
+++ src/service/test/SimpleEvaluatorTest.cpp
@@ -20,11 +20,13 @@ TEST_CASE("SimpleEvaluator") {
 
     // Below, test sections should use the evaluator exclusively.
 
-    SECTION("binary operators") {
+    SECTION("binary operators (normal)") {
         REQUIRE("x^2\n" == evaluator->evaluate("1+x^2-1"));
         REQUIRE("x^2-1\n" == evaluator->evaluate("(x+1) * (x - 1)"));
         REQUIRE("x^3-3x^2+3x-1\n" == evaluator->evaluate("(x-1) ^ 3"));
         REQUIRE("1\n" == evaluator->evaluate("(2*x - 1)^0"));
         REQUIRE("1\n" == evaluator->evaluate("(2*x - 1)^(x - x)"));
+        REQUIRE("2x-1\n" == evaluator->evaluate("(2*x - 1)^(x'1 + x - x)"));
+        REQUIRE("20x^3\n" == evaluator->evaluate("(x^5)'2"));
     }
 }
